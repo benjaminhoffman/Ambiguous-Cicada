045824f11aff78b07bda28362f14e99626823bf6
Benjamin <bhoffman8@gmail.com>
2015-10-08 16:07:12 -0700

    deletes commented code

--- client-mobile/www/js/profile.js
+++ client-mobile/www/js/profile.js
@@ -2,21 +2,6 @@ angular.module('kwiki.profile', [])
 
 .factory('ProfileFactory', function ($http, $state, SocketFactory, $window, $rootScope) {
 
-  // var getProfileInfo = function(userProfile) {
-
-  //   return $http({
-  //     method: 'GET',
-  //     url: $rootScope.host + '/profile/' + userProfile.id,
-  //     data: userProfile
-  //   })
-  //   .then(function(res) {
-  //     return res.data;
-  //   })
-  //   .catch(function(err) {
-  //     console.log(err);
-  //   });
-  // };
-
   var updateProfileInfo = function(userProfile) {
     return $http({
       method: 'PUT',
@@ -32,25 +17,11 @@ angular.module('kwiki.profile', [])
   };
 
   return {
-    // getProfileInfo: getProfileInfo,
     updateProfileInfo: updateProfileInfo
   }
 })
 
 .controller('ProfileCtrl', function ($rootScope, $state, $scope, ProfileFactory, AuthFactory) {
-
-
-  // ProfileFactory.getProfileInfo($scope.user)
-  //   .then(function(res) {
-  //     console.log(res)
-  //     $scope.user.interests = {
-  //       sports: res.sports
-  //     }
-
-  //   })
-  //   .catch(function(err) {
-  //     console.log(err)
-  //   });
 
   $scope.handleSubmit = function(userProfile) {
 
