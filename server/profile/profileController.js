9a1b852c0d20d3ff66abac4961d67107b1908bad
Benjamin <bhoffman8@gmail.com>
2015-10-07 17:26:55 -0700

    removes console logs

--- server/profile/profileController.js
+++ server/profile/profileController.js
@@ -21,42 +21,29 @@ module.exports = {
         res.send(err);
       }
 
-      // logs the data coming in from the PUT req:
-        console.log("USER'S GENDER: ")
-        console.log(req.body.gender)
-        console.log("USER'S PREF GENDER: ")
-        console.log(req.body.prefGender)
-        console.log("USER'S INTERESTS: ")
-        console.log(req.body.interests)
-
       // if user updates their gender, update the db
       if (req.body.gender) {
         profile.gender = req.body.gender;
-        console.log('gender updated!')
       }
 
       // if user updates their gender preference, update the db
       if (req.body.prefGender) {
         profile.prefGender = req.body.prefGender;
-        console.log('prefGender updated!')
       }
 
       // if indicates they like (or don't like) sports, update the db
       if (req.body.interests.sports) {
         profile.sports = req.body.interests.sports;
-        console.log('sports updated!')
       }
       
       // if indicates they like (or don't like) beauty, update the db
       if (req.body.interests.beauty) {
         profile.beauty = req.body.interests.beauty;
-        console.log('beauty updated!')
       }
       
       // if indicates they like (or don't like) other, update the db
       if (req.body.interests.other) {
         profile.other = req.body.interests.other;
-        console.log('other updated!')
       }
       
       // callback method to be run after our db is updated
